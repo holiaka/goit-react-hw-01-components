7244502558495e2e4d17d959740f5ecabc45ef6b
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2023-01-07 09:43:58 +0200

    Added  "List" to FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,18 +1,20 @@
 import PropTypes from 'prop-types';
+import {List} from './FriendList.styled'
 
 export const FriendList = ({ friends }) => {
-    return (
-        <ul>
-            {friends.map(friend => (
-                <li key={friend.id}></li>))}
-        </ul>
-    );
+  return (
+    <List>
+      {friends.map(friend => (
+        <li key={friend.id}>Hi!</li>
+      ))}
+    </List>
+  );
 };
 
 FriendList.propTypes = {
-    friends: PropTypes.arrayOf(
+  friends: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
     })
   ).isRequired,
-}
+};
